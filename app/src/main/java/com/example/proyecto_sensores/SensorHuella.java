ca835fd11a4c6298c148b9d2ef20b3e283b7414b
Hernán Fonseca <hfonseca@excibit-corp.com>
2020-12-06 15:37:24 -0500

    Esqueleto, esto no sirve todavía

---
+++ app/src/main/java/com/example/proyecto_sensores/SensorHuella.java
@@ -0,0 +1,70 @@
+package com.example.proyecto_sensores;
+
+import android.app.Activity;
+import androidx.biometric.BiometricPrompt;;
+import android.os.Build;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import java.util.concurrent.Executor;
+
+public class SensorHuella extends AppCompatActivity {
+    public static BiometricPrompt biometricPrompt;
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    public void SensorHuella(){
+        Executor executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),
+                        "Ta cool!", Toast.LENGTH_SHORT).show();
+                //startActivity(new Intent(getApplicationContext(), TipoPedido.class));
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setSubtitle("Log in using your biometric credential")
+                .setNegativeButtonText("Use account password")
+                .build();
+
+        // Prompt appears when user clicks "Log in".
+        // Consider integrating with the keystore to unlock cryptographic operations,
+        // if needed by your app.
+        Button biometricLoginButton = findViewById(R.id.btnFinger);
+        biometricLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                biometricPrompt.authenticate(promptInfo);
+
+                //startActivity(new Intent(view.getContext(), TipoPedido.class));
+            }
+        } );
+    }
+
+}
