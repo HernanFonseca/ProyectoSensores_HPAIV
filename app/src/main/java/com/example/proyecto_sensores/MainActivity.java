a1ff16497045c3252dd9788855e9811e04ba518e
lcaliu23 <lucia.liu@utp.ac.pa>
2020-12-06 20:55:43 -0500

    nada de papayas (espero)

--- app/src/main/java/com/example/proyecto_sensores/MainActivity.java
+++ app/src/main/java/com/example/proyecto_sensores/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.proyecto_sensores;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
-    TextView latitud, longitud;
+    TextView txtLatitud, txtLongitud;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +41,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        latitud = findViewById(R.id.txtLatitud);
-        longitud = findViewById(R.id.txtLongitud);
-
-        SensorGPS gps = new SensorGPS(getApplicationContext(), MainActivity.this, latitud, longitud);
+        txtLatitud = findViewById(R.id.txtLatitud);
+        txtLongitud = findViewById(R.id.txtLongitud);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        try {
+            SensorGPS gps = new SensorGPS(getApplicationContext(),
+                    this, locationManager, txtLatitud, txtLongitud);
+        } catch(Exception e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/example/proyecto_sensores/SensorGPS.java
+++ app/src/main/java/com/example/proyecto_sensores/SensorGPS.java
@@ -8,34 +8,39 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-public class SensorGPS {
-    LocationManager locationManager;
+public class SensorGPS extends AppCompatActivity {
     TextView txtLatitud, txtLongitud;
 
-    public SensorGPS(Context context, Activity activity, TextView ltt, TextView lgt) {
+    public SensorGPS(Context context, Activity activity, LocationManager locationManager, TextView ltt, TextView lgt) {
         txtLatitud = ltt; txtLongitud = lgt;
 
-        if(ContextCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(context,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(activity, new String[] {
-                    Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
-                    1);
-        }
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                10, 1, new LocationListener() {
-            @Override
-            public void onLocationChanged(@NonNull Location location) {
-                txtLatitud.setText(String.valueOf(location.getLatitude()));
-                txtLongitud.setText(String.valueOf(location.getLongitude()));
+        try {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(context,
+                            Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, new String[]{
+                                Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
             }
-        });
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000, 1, new LocationListener() {
+                        @Override
+                        public void onLocationChanged(@NonNull Location location) {
+                            txtLatitud.setText(String.format("%.6f", location.getLatitude()));
+                            txtLongitud.setText(String.format("%.6f", location.getLongitude()));
+                        }
+                    });
+        } catch(Exception e) {
+            Toast.makeText(context, "ERROR: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
